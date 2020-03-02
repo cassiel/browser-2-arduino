3435b4842ec1e47d2e3d35bb210d27e68d5b43d0
Nick Rothwell <NRothwell@c02stemsgtfl.client.rave.ac.uk>
2020-03-02 14:48:18 +0000

    Working example (not documented).

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # browser-2-arduino
 Two-way communication between web browser and Arduino via websockets and Node.js serial proxy.
+
+- Do an `npm install socket.io` in `proxy`.
+- Also: `npm install express` for the proxy's web/socket server.
---
+++ arduino/LED-demo/LED-demo.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+long lastMillis = 0;
+int counter = 0;
+
+void loop() {
+  while (Serial.available() > 0) {
+    int x = Serial.read();
+    if (x == 'X') {
+      digitalWrite(13, 1);
+    } else {
+      digitalWrite(13, 0);
+    }
+  }
+
+  if (millis() > lastMillis + 1500) {
+    lastMillis = millis();
+    // Single-character ASCII output, A to J:
+    Serial.write('A' + counter);
+    counter = (counter + 1) % 10;
+  }
+}
